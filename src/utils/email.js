0c3c68eefb88eb5b0c4532793f023f0f89c777b3
Chris <chris@straza.co.uk>
2018-03-04 22:17:44 +0000

    Return actual response from email client

--- src/utils/email.js
+++ src/utils/email.js
@@ -4,13 +4,14 @@ const config = require('enviro-conf');
 const TO_EMAIL = config.get('TO_EMAIL');
 const SENDGRID_API_KEY = config.get('SENDGRID_API_KEY');
 
-module.exports = content => {
+module.exports = async content => {
   sgMail.setApiKey(SENDGRID_API_KEY);
   const msg = {
-    to: TO_EMAIL,
+    to: TO_EMAIL.split(','),
     from: 'train@delays',
     subject: 'New Delays',
     html: content,
   };
-  return sgMail.send(msg);
+  const result = await sgMail.send(msg);
+  return result[0];
 };
